96b3475064fe745247aba0bfa4cd225fadcda758
Ir Benjamin <benjaminiriganje@gmail.com>
2026-03-09 23:17:38 +0200

    Add a comment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,75 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+    {   
+        Console.Write("What is your percentage?"); 
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string Letter = "";
+        
+       string Sign = "";
+        
+        if(percentage >= 90)
+        {
+             Letter = "A";
+        }
+       
+        else if (percentage >= 80)
+        {
+            Letter = "B";;
+        }
+        else if (percentage >= 70)
+        {
+            Letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+        int lastdigit = percentage % 10;
+
+        if(lastdigit >= 7)
+        {
+           Sign ="+"; 
+        }
+        else if(percentage < 3)
+        {
+            Sign = "-";
+        }
+        else
+        {
+            Sign = "";
+        }
+        
+        if(Letter == "A" && Sign == "+")
+        {
+            Sign = "";
+        }
+        
+        if(Letter== "F")
+        {
+            Sign = "";
+        }
+
+        Console.WriteLine($"Your note is : {Letter}{Sign}");
+
+        if(percentage >= 70)
+        {
+            Console.WriteLine("congratulations, you have passed the course successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Don't get discouraged, keep up your efforts for next time!");
+        }
     }
+
+    
 }
