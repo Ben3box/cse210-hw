47bc8c7e08416e20bbad0d0b566afd6a1586ecaf
Ir Benjamin <benjaminiriganje@gmail.com>
2026-03-15 20:36:41 +0200

    submission

--- week02/Journal/journal.cs
+++ week02/Journal/journal.cs
@@ -17,12 +17,30 @@ public class Journal
     }
 
     public void SaveToFile(string file)
+{
+    using (StreamWriter output = new StreamWriter(file))
     {
-        
+        foreach (Entry entry in _entries)
+        {
+            output.WriteLine($"{entry.Date}|{entry.PromptText}|{entry.entrytext}");
+        }
     }
+}
 
     public void LoadFromFile(string file)
+{
+    string[] lines = File.ReadAllLines(file);
+
+    foreach (string line in lines)
     {
-        
+        string[] parts = line.Split("|");
+
+        Entry entry = new Entry();
+        entry.Date = parts[0];
+        entry.PromptText = parts[1];
+        entry.entrytext = parts[2];
+
+        _entries.Add(entry);
     }
+}
 }
