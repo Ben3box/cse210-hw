43335385bce826f59d109eb4a495bcc59221a41e
Ir Benjamin <benjaminiriganje@gmail.com>
2026-04-07 17:48:20 +0200

    first commit

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Math Assignment
+        MathAssignment math = new MathAssignment(
+            "John Smith",
+            "Fractions",
+            "7.3",
+            "3-10, 20-21"
+        );
+
+        Console.WriteLine("Math Assignment:");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // Writing Assignment
+        WritingAssignment writing = new WritingAssignment(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+
+        Console.WriteLine("Writing Assignment:");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
